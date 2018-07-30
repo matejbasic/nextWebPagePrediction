a8a6f2c156d6f21e415f833e7b83d0e2039c2fef
ping <matej@neuralab.net>
2018-07-31 00:37:14 +0200

    results & conclusion refactored

--- requirements.sh
+++ requirements.sh
@@ -1,12 +1,12 @@
 #! bin/bash
 
 declare -a pip_installs=("oauth2client" "httplib2" "google-api-python-client"
-                         "numpy" "scipy" "scikit-learn" "matplotlib"
+                         "numpy" "scipy" "scikit-learn" "matplotlib" "pandas"
                         )
 
 for i in "${pip_installs[@]}"
 do
-  pip install --upgrade $i
+  pip install $i
 done
 
 echo "END"
